4dee8f41b3ef2d217cf5deeb521e0a63c928a81d
Mike <micpe@senmatic.com>
2017-09-26 09:14:46 +0200

    update to player: - added movement - added speed

---
+++ Assets/Scripts/player_controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float move_horizontal = Input.GetAxis("Horizontal");
+        float move_vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(move_horizontal, 0, move_vertical); 
+
+        rb.AddForce(movement * speed);
+    }
+}
